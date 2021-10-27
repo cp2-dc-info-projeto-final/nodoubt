b90a151e7ba247da64a9497b1f3af68d88f25d0a
Vicky do nascimento <65239807+VickyNascimento@users.noreply.github.com>
2021-10-27 12:52:20 -0300

    Recebedados

--- src/Login/Recebedados.php
+++ src/Login/Recebedados.php
@@ -7,31 +7,37 @@
             $erro = 0;
 
             if(strlen($username) < 5){
-                echo "O username deve possuir no mínimo 5 caracteres.<br>";
+                
+                echo "O campo: Nome de Usuario deve possuir no mínimo 5 caracteres.<br>";
                 $erro = 1;
                 }
              if(strlen($senha) < 5){
-                echo "A senha deve possuir no mínimo 5 caracteres.<br>";
+                echo "O campo: Senha deve possuir no mínimo 5 caracteres.<br>";
                 $erro = 1;
                 }
              if($username == $senha){
-                echo "O username e a senha devem ser diferentes.<br>";
+                echo "O campo: Nome de Usuario e Senha devem ser diferentes.<br>";
                 $erro = 1;
                 }
             if(empty($nome) OR strstr($nome,' ') == FALSE){    
-                echo "Favor digitar seu nome corretamente. <br>";
+                echo "O campo: Nome esta vazio ou foi inserido de forma incorreta. <br>";
                 $erro = 1;
                 }
             if(is_numeric($idade) == FALSE){
-                echo "Favor digitar sua idade corretamente.<br>";
+                echo "O campo: Idade deve possui somente numeros.<br>";
                 $erro = 1;
                 }
             if(strlen($email) < 8 || strstr($email,'@') == FALSE){
-                echo "Favor digitar seu email corretamente.<br>";
+                echo "O campo: E-mail não foi digitado corretamente.<br>";
                 $erro = 1;
                 }
                 // VERIFICA SE NÃO HOUVE ERRO
              if($erro == 0) {
-                echo "Todos os dados foram digitados corretamente!";
+                echo "Todos os dados foram digitados corretamente!<br>";
+                echo "Cadastro completo.";
                 }
+
+               if($erro == 1) {
+                    echo"<br>Cadastro incompleto.<br>";
+               }
         ?>
