5554f8161921cdaab47a4dce7ca1dbb6821d9bed
Vicky do nascimento <65239807+VickyNascimento@users.noreply.github.com>
2021-10-29 19:33:16 -0300

    autenticacao

---
+++ src/Login/Confirma.php
@@ -0,0 +1,26 @@
+<?php
+        $email = $_POST["email"];
+        $senha = $_POST["senha"];
+
+        include "conecta_mysql.inc";
+            $sql ="SELECT * FROM usuarios WHERE username ='$username';";
+            $res = mysqli_query($mysqli, $sql);
+                if(mysqli_num_rows($res) != 1){
+                    echo "Username inválido!";
+                    echo "<p><a href='login.html'>Página de login</a></p>";
+        }
+            else{
+                $usuario = mysqli_fetch_array($res);
+                if($senha != $usuario["senha"]){
+                    echo "Senha inválida!";
+                    echo "<p><a href='login.html'>Página de login</a></p>";
+        }
+            else{ // usuário e senha corretos, abre a sessão e registra as variáveis
+            session_start();
+            $_SESSION["username"] = $username;
+            $_SESSION["senha"] = $senha;
+            header("Location: index.php");
+            }
+        }
+        mysqli_close($mysqli);
+?>
