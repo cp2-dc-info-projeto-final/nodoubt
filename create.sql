943ebfd632206ab0cc5d52d466e1ef22a876f5c5
Vicky do nascimento <65239807+VickyNascimento@users.noreply.github.com>
2021-11-01 11:58:28 -0300

    create.sql

--- create.sql
+++ create.sql
@@ -1,1 +1,12 @@
+CREATE TABLE cadastrousuarios (
+    codusuario int NOT NULL AUTO_INCREMENT,
+    usernameusuario varchar(12) NOT NULL,
+    senhausuario varchar(255) NOT NULL,
+    nomeusuario varchar(40) NOT NULL,
+    idadeusuario int(3) NOT NULL,
+    emailusuario varchar(30) NOT NULL,
+    primary key (codusuario)
+);
 
+CREATE USER'estudante'@'localhost'IDENTIFIED BY '12345';
+GRANT ALL PRIVILEGES ON cadastro.* TO 'estudante'@'localhost';
