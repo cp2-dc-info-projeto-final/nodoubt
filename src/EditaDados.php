c9ddd4007bf903aced601195317cd8d5776f3d5e
Vicky do nascimento <65239807+VickyNascimento@users.noreply.github.com>
2021-11-08 19:28:05 -0300

    att

--- src/EditaDados.php
+++ src/EditaDados.php
@@ -2,30 +2,40 @@
 <?php
     include "autentica.inc";
     include "conecta_mysql.inc";
-    $emaiu = $_GET["emailusuario"];
-    $sql = "SELECT * FROM cadastrousuarios WHERE emailusuario = '$emaiu';";
+    $emailuser = $_GET["emailusuario"];
+    $sql = "SELECT * FROM cadastrousuarios WHERE emailusuario = '$emailuser';";
     $res = mysqli_query($mysqli,$sql);
+    $linhas = mysqli_num_rows($res);
+    for($i=0; $i < $linhas; $i++){
     $usuario = mysqli_fetch_array($res);
-    if($usuario["permissadm"] == 1)
-    {
-        $permiss = $usuario["permissadm"];
-    }
-
+    $permiss = $usuario["permissadm"];
+}
 ?>
 
 <p><strong>Editar Dados</strong></p>
 <form action="Recebedados.php" method="POST">
     <input type="hidden" name="operacao" value="atualizar">
-    <input type="hidden" name="emailusuario" value="<?php echo $emaiu?>">
+    <input type="hidden" name="emailusuario" value="<?php echo $emailuser?>">
     <p>Nome de usuario: <input type="text" name="username" size="10" value="<?php echo $usuario["usernameusuario"]?>"> </p>
     <p>Senha: <input type="password" name="senha" size="10" value="<?php echo $usuario["senhausuario"]?>"> </p>
     <p>Nome: <input type="text" name="nome" size="30" value="<?php echo $usuario["nomeusuario"]?>"> </p>
     <p>Idade: <input type="text" name="idade" size="3" value="<?php echo $usuario["idadeusuario"]?>"> </p>
     <p>E-mail: <input type="text" name="email" size="30" value="<?php echo $usuario["emailusuario"]?>"></p>
     <?php 
-    if($permiss == 1){
-        ?> <p>Permissão: <imput type="text" name="permiss" size="2" value="<?php echo $usuario["permissadm"]?>"></p>
+        include "conecta_mysql.inc";
+        $emailuser = $_GET["emailusuario"];
+        $sql = "SELECT * FROM cadastrousuarios WHERE emailusuario = '$emailuser';";
+        $res = mysqli_query($mysqli,$sql);
+        $linhas = mysqli_num_rows($res);
+        for($i=0; $i < $linhas; $i++){
+        $usuario = mysqli_fetch_array($res);
+        if($["permissadm"] == 1){
+            ?>
+          <p><a href='IndexAdm.php'>Usuarios</a></p>
+            <?php
+        }
     }
+    ?>
     <p><input type="submit" value="Enviar!"></p>
 </form>
 <?php
