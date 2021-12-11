dd3632e0183f172ff97886219788aa0a4cc00088
Vicky do nascimento <65239807+VickyNascimento@users.noreply.github.com>
2021-12-11 12:34:29 -0300

    att

--- src/editapost.php
+++ src/editapost.php
@@ -12,7 +12,7 @@
 
         <meta name="viewport"content="width=device-width, initial-scale=1.0">
 
-        <link rel="stylesheet" href="estilização.css">
+        <link rel="stylesheet" href="estilizar.css">
 
         <title>Editar post</title>
 
