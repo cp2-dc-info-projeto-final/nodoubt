d36807543a1444f3e15e97edf57ea8c27eab2aa0
vitoriamaria 31 <vitoriacostah31@gmail.com>
2021-12-11 12:40:32 -0300

    att

--- src/EditaDados.php
+++ src/EditaDados.php
@@ -20,7 +20,7 @@
                 <div class="card-content-area">
             <form action="Recebedados.php" method="POST">
                 <input type="hidden" name="emailusuario" value="<?php echo $emailuser?>">
-                <p>Nome de usuario: <input type="text" name="username" size="10" value="<?php echo $usuario["usernameusuario"]?>"> </p>
+                <p>Nickname: <input type="text" name="username" size="10" value="<?php echo $usuario["usernameusuario"]?>"> </p>
                 <p>Nome: <input type="text" name="nome" size="30" value="<?php echo $usuario["nomeusuario"]?>"> </p>
                 <p>Idade: <input type="text" name="idade" size="3" value="<?php echo $usuario["idadeusuario"]?>"> </p>
                 <p>E-mail: <input type="text" name="email" size="30" value="<?php echo $usuario["emailusuario"]?>"></p>
