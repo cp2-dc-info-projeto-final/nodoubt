5fbe1b283426588cbb443ca0784086ea8fa5bbba
Vicky do nascimento <65239807+VickyNascimento@users.noreply.github.com>
2021-12-11 21:01:59 -0300

    att

--- src/Perfil.php
+++ src/Perfil.php
@@ -127,7 +127,7 @@ $nome = $usuario["usernameusuario"];
       <input type="hidden" name="user" value="<?php echo $coduser?>"></input>
       <input type="hidden" name="iduser" value="<?php echo $iduser?>"></input>
       <i><h5>Título</h5></i>
-      <input type="text" name="Título" size="25" placeholder="Título"><p>
+      <input type="text" name="titulo" size="25" placeholder="Título"><p>
       <i><h5>Postagem</h5></i>
       <textarea type="text" name="post" placeholder="Qual sua dúvida?" cols="55" rows="3"></textarea><br><br>
       <input type="submit" class="submit" value="Postar!">
